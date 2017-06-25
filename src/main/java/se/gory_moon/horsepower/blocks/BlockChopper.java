7e3847e040b1051cd9781bfc8f5bf71c93934dd3
GoryMoon <gurreja@gmail.com>
2017-06-25 13:51:53 +0200

    Fixed issue with chopping block placement not saving and on servers is wasn't syncing on placement

--- src/main/java/se/gory_moon/horsepower/blocks/BlockChopper.java
+++ src/main/java/se/gory_moon/horsepower/blocks/BlockChopper.java
@@ -7,7 +7,6 @@ import mcjty.theoneprobe.api.ProbeMode;
 import mcjty.theoneprobe.apiimpl.styles.ProgressStyle;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
@@ -19,13 +18,11 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.common.property.ExtendedBlockState;
-import net.minecraftforge.common.property.IExtendedBlockState;
-import net.minecraftforge.common.property.IUnlistedProperty;
 import net.minecraftforge.fml.common.Optional;
 import se.gory_moon.horsepower.Configs;
 import se.gory_moon.horsepower.client.renderer.ChopperModels;
@@ -36,14 +33,13 @@ import se.gory_moon.horsepower.util.Colors;
 import se.gory_moon.horsepower.util.Localization;
 
 import javax.annotation.Nullable;
-import java.util.Arrays;
 import java.util.List;
 
 @Optional.Interface(iface = "mcjty.theoneprobe.api.IProbeInfoAccessor", modid = "theoneprobe")
 public class BlockChopper extends BlockHPBase implements IProbeInfoAccessor {
 
-    public static final UnlistedDirection FACING = new UnlistedDirection("facing");
-    public static final PropertyDirection DIRECTION = PropertyDirection.create("facing", Arrays.asList(EnumFacing.HORIZONTALS));
+    //TODO add in future, causing bug when reenter ", Arrays.asList(EnumFacing.HORIZONTALS)"
+    public static final PropertyDirection FACING = PropertyDirection.create("facing");
     public static final PropertyEnum<ChopperModels> PART = PropertyEnum.create("part", ChopperModels.class);
 
     private static final AxisAlignedBB COLLISION_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 2.0D, 1.0D);
@@ -87,12 +83,7 @@ public class BlockChopper extends BlockHPBase implements IProbeInfoAccessor {
 
     @Override
     protected BlockStateContainer createBlockState() {
-        return new ExtendedBlockState(this, new IProperty[] {PART, DIRECTION}, new IUnlistedProperty[]{FACING});
-    }
-
-    @Override
-    public int getMetaFromState(IBlockState state) {
-        return 0;
+        return new BlockStateContainer(this, PART, FACING);
     }
 
     @Override
@@ -101,16 +92,45 @@ public class BlockChopper extends BlockHPBase implements IProbeInfoAccessor {
         if (tile == null)
             return state;
 
-        return ((IExtendedBlockState) state).withProperty(FACING, tile.getForward()).withProperty(DIRECTION, tile.getForward()).withProperty(PART, state.getValue(PART));
+        return state.withProperty(FACING, tile.getForward()).withProperty(PART, state.getValue(PART));
+    }
+
+    @Override
+    public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
+        if (!worldIn.isRemote) {
+            EnumFacing enumfacing = state.getValue(FACING);
+            worldIn.setBlockState(pos, state.withProperty(FACING, enumfacing).withProperty(PART, ChopperModels.BASE), 2);
+        }
+    }
+
+    @Override
+    public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer, EnumHand hand) {
+        return getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite()).withProperty(PART, ChopperModels.BASE);
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return state.getValue(FACING).getIndex();
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        EnumFacing enumfacing = EnumFacing.getFront(meta);
+
+        if (enumfacing.getAxis() == EnumFacing.Axis.Y) {
+            enumfacing = EnumFacing.NORTH;
+        }
+
+        return getDefaultState().withProperty(FACING, enumfacing).withProperty(PART, ChopperModels.BASE);
     }
 
     public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
-        worldIn.setBlockState(pos, ((IExtendedBlockState)state).withProperty(FACING, placer.getHorizontalFacing().getOpposite()).withProperty(DIRECTION, placer.getHorizontalFacing().getOpposite()), 2);
+        worldIn.setBlockState(pos, state.withProperty(FACING, placer.getHorizontalFacing().getOpposite()), 2);
 
         TileEntityHPBase tile = getTileEntity(worldIn, pos);
         if (tile == null)
             return;
-        tile.setForward(placer.getAdjustedHorizontalFacing().getOpposite());
+        tile.setForward(placer.getHorizontalFacing().getOpposite());
     }
 
     @Override
